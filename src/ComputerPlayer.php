56326d369825481ea5338277c525de01a8591b7d
katyador57d-cmd <katyador57d@gmail.com>
2026-05-24 20:20:06 +0200

    feat: implement Player hierarchy

--- src/ComputerPlayer.php
+++ src/ComputerPlayer.php
@@ -1,2 +1,18 @@
 <?php
+
 declare(strict_types=1);
+
+class ComputerPlayer extends Player {
+    public function __construct(string $symbol)
+    {
+        parent::__construct($symbol);
+    }
+
+    public function makeMove(Board $board): array
+    {
+        $emptyCells = $board->getEmptyCells();
+        $randomIndex = array_rand($emptyCells);
+        return $emptyCells[$randomIndex];
+
+    }
+}
--- src/HumanPlayer.php
+++ src/HumanPlayer.php
@@ -1,2 +1,37 @@
 <?php
+
 declare(strict_types=1);
+
+class HumanPlayer extends Player {
+
+    
+    public function __construct(
+        string $symbol,
+        private readonly Display $display
+    )
+    {
+        parent::__construct($symbol);
+    }
+    public function makeMove(Board $board): array
+    {
+        while (true) {
+            echo "Введите координаты: row-col, 0-2" . "\n";
+            $input = trim(fgets(STDIN));
+
+            $coord = explode(" ", $input);
+            if (count($coord) !== 2) {
+                $this->display->printError("Введите 2 числа!");
+                continue;
+            }
+
+        $row = (int)$coord[0];
+        $col = (int)$coord[1];
+        
+        if (!$board->isCellEmpty($row, $col)) {
+            $this->display->printError("Клетка занята!");
+            continue;
+        }
+        return [$row, $col];
+    }
+    }
+}
